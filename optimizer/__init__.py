a39321475b2acc439d11f31214544a11ac6a7986
asdadas1113 <crony06@gmail.com>
2026-08-30 23:49:28 +0900

    feat: export account snapshot API

--- optimizer/__init__.py
+++ optimizer/__init__.py
@@ -1,5 +1,12 @@
 """Roster optimizer prototype built around the Moris simulator evaluator."""
 
+from .account import (
+    AccountSnapshot,
+    AccountSyncAdapter,
+    FieldProvenance,
+    ProvenanceStatus,
+    normalize_account_sync,
+)
 from .candidates import CandidateTeam
 from .constraints import (
     BurstMetadata,
@@ -21,6 +28,8 @@ from .synergy import PairSynergyObservation, PairSynergyProbe, measure_pair_prob
 from .validation import ValidationMetrics, enumerate_legal_teams, run_exhaustive_validation
 
 __all__ = [
+    "AccountSnapshot",
+    "AccountSyncAdapter",
     "Allocation",
     "BurstMetadata",
     "BurstStructureReport",
@@ -31,14 +40,17 @@ __all__ = [
     "Evaluation",
     "EvaluationTimings",
     "EvaluatorStats",
+    "FieldProvenance",
     "MorisEvaluator",
     "OneSwapNeighbor",
     "PairSynergyObservation",
     "PairSynergyProbe",
+    "ProvenanceStatus",
     "ValidationMetrics",
     "enumerate_legal_teams",
     "generate_one_swap_neighbors",
     "measure_pair_probes",
+    "normalize_account_sync",
     "run_exhaustive_validation",
     "select_global_allocation",
     "teams_are_disjoint",
